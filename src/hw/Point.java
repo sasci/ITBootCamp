0a1f011e306dcff0d3b3e8be0e568b617ef399e3
sasci <tunca0173@gmail.com>
2020-02-06 09:47:45 +0000

    wall and point classes is added

---
+++ src/hw/Point.java
@@ -0,0 +1,41 @@
+package hw;
+
+public class Point {
+	private int x;
+	private int y;
+	
+	public Point() {}
+	
+	public Point(int x,int y) {
+		this.x=x;
+		this.y=y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public double distance() {
+		return Math.sqrt(Math.pow(x, 2)+Math.pow(y, 2));
+	}
+	
+	public double distance(int x,int y) {
+		return Math.sqrt(Math.pow((x-this.x), 2)+Math.pow((y-this.y), 2));
+	}
+	
+	public double distance(Point p) {
+		return Math.sqrt(Math.pow((p.x-this.x), 2)+Math.pow((p.y-this.y), 2));
+	}
+}
